62e0a50f0efa5a710c34be036907a92b4d225c0d
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-10-28 03:27:59 -0700

    tiramisu problem initial checkin

---
+++ assignment1/tiramisu.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+PERSON = ["James", "Daniel", "Emily", "Sophie"]
+STARTER = ["Prawn_Cocktail", "Onion_Soup", "Mushroom_Tart", "Carpaccio"]
+MAINCOURSE = ["Baked_Mackerel", "Fried_Chicken", "Filet_Steak", "Vegan_Pie"]
+DRINK = ["Red_Wine", "Beer", "White_Wine", "Coke"]
+DESSERT = ["Apple_Crumble", "Ice_Cream", "Chocolate_Cake", "Tiramisu"]
+
+

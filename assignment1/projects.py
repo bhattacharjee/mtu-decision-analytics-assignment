a26799e5e0860d1f7ed921a11a056a9f0cde09de
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-10-29 05:58:50 -0700

    work in progress

--- assignment1/projects.py
+++ assignment1/projects.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+from ortools.sat.python import cp_model
 
 class Project:
 
@@ -14,8 +15,14 @@ class Project:
         self.contractor_names = None
         self.month_names = None
         self.excel_file = excel_file
+        self.model = cp_model.CpModel()
+
+        # One variable per project to indicate whether it is picked up or not
+        self.project_vars = {}
+
 
         self.read_excel(self.excel_file)
+        self.create_project_variables_and_constraints()
 
     def read_excel(self, excelfile:str) -> None:
         self.projects_df = pd.read_excel(excelfile, sheet_name='Projects')
@@ -38,6 +45,39 @@ class Project:
         print(f"Month Names     : {self.month_names}")
         print(f"Job Names       : {self.job_names}")
         print(f"Contractor Names: {self.contractor_names}")
+
+        print(self.depend_df)
+
+    def create_project_variables_and_constraints(self):
+        for p in self.project_names:
+            self.project_vars[p] = self.model.NewBoolVar(f"{p}")
+
+        def add_required_dependency(p1:str, p2:str)->None:
+            # p1 implies p2
+            self.model.AddBoolOr(                                           \
+                    [                                                       \
+                        self.project_vars[p1].Not(),                        \
+                        self.project_vars[p2]                               \
+                    ])
+
+        def add_conflict_dependency(p1:str, p2:str)->None:
+            self.model.AddBoolOr(                                           \
+                    [                                                       \
+                        self.project_vars[p1].Not(),                        \
+                        self.project_vars[p2].Not()                         \
+                    ])
+
+        for p1 in self.project_names:
+            for p2 in self.project_names:
+                row_p1 = self.depend_df[self.depend_df['Project'] == p1]
+                e_p1_p2 = row_p1[p2].tolist()[0]
+                if (isinstance(e_p1_p2, str)):
+                    if ('required' == e_p1_p2.lower()):
+                        add_required_dependency(p1, p2)
+                    if ('conflict' == e_p1_p2.lower()):
+                        add_conflict_dependency(p1, p2)
+
+        print(self.depend_df)
 
 
 def main():

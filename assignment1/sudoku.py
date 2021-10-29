a40f0befa2224da2afca0329b2a2c3ab9dbb629d
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-10-29 06:05:00 +0100

    checkin formatting

--- assignment1/sudoku.py
+++ assignment1/sudoku.py
@@ -65,20 +65,23 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
         self.solutions = self.solutions + 1
 
         print(f"Solution # {self.solutions}")
-        print("++=======+===+===+===+===+===+===+===+===+===++")
-        print("||   #   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 ||")
-        print("++=======+===+===+===+===+===+===+===+===+===++")
+        print("++=======++===+===+===+===+===+===+===+===+===++")
+        print("||   #   || 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 ||")
+        print("++=======++===+===+===+===+===+===+===+===+===++")
 
         for i in range(9):
             output_line = f"||   {i}  "
+            first = True
             for j in range(9):
                 for k in numbers():
                     if self.Value(self.sudoku[i][j][k]):
-                        output_line = output_line + f" | {k}"
+                        addstr = f" || {k}" if first else f" | {k}"
+                        first = False
+                        output_line = output_line + addstr
                         break
             output_line = output_line + f" ||"
             print(output_line)
-            print("++-------+---+---+---+---+---+---+---+---+---++")
+            print("++-------++---+---+---+---+---+---+---+---+---++")
 
         print()
         print()

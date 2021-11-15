412008da914e0d471af6a8965a6f599135013427
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-11-15 12:55:53 -0800

    argument parsing

--- assignment1/runner.py
+++ assignment1/runner.py
@@ -4,8 +4,30 @@ from tiramisu2 import *
 from projects import *
 from sudoku import *
 
+import argparse
+
 def main():
-    tiramisu_main()
+    parser = argparse.ArgumentParser("Decision Analytics Assignment 1")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(                                                     \
+            '-s',                                                           \
+            '--sudoku',                                                     \
+            help='Solve the sudoku for the given problem',                  \
+            action='store_true')
+    group.add_argument(                                                     \
+            '-t',                                                           \
+            '--tiramisu',                                                   \
+            help='Solve the tiramisu problem as specified',                 \
+            action='store_true')
+    group.add_argument(                                                     \
+            '-p',                                                           \
+            '--projects',                                                   \
+            help='Solve the project planning problem, ' +                   \
+                    'assumes excel file in the same directory',             \
+            action='store_true')
+    args = parser.parse_args()
+    if args.sudoku:
+        tiramisu_main()
 
 if "__main__" == __name__:
     main()

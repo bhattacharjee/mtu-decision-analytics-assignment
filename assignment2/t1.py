93775a9e6c5782e1697a624df00bcdc3a582b192
Rajbir Bhattacharjee <r.bhattacharjee@eclecticiq.com>
2021-12-09 15:09:37 +0000

    some more boilerplate code

--- assignment2/t1.py
+++ assignment2/t1.py
@@ -7,22 +7,63 @@ from ortools.linear_solver import pywraplp
 class Task1():
     def __init__(self, excel_file_name:str):
         self.excel_file_name = excel_file_name
+
         self.supplier_stock_df = self.read_csv("Supplier stock")
+        self.replace_nans(self.supplier_stock_df, 0)
+
         self.raw_materials_stock_df = self.read_csv("Raw material costs")
+        self.replace_nans(self.raw_materials_stock_df, 'inf')
+
         self.raw_material_shipping_df = self.read_csv("Raw material shipping")
+        self.replace_nans(self.raw_material_shipping_df, 'inf')
+
         self.product_requirements_df = self.read_csv("Product requirements")
+        self.replace_nans(self.product_requirements_df, 0)
+
         self.production_capacity_df = self.read_csv("Production capacity")
+        self.replace_nans(self.production_capacity_df, 0)
+
         self.production_cost_df = self.read_csv("Production cost")
+        self.replace_nans(self.production_cost_df, 'inf')
+
         self.customer_demand_df = self.read_csv("Customer demand")
-        self.shipping_cost_df = self.read_csv("Shipping costs")
+        self.replace_nans(self.customer_demand_df, 0)
+
+        self.shipping_costs_df = self.read_csv("Shipping costs")
+        self.replace_nans(self.shipping_costs_df, 'inf')
+        
+        self.product_names = [str(x) for x in \
+            self.product_requirements_df['Unnamed: 0']]
+
+        self.supplier_names = [str(x) for x in \
+            self.supplier_stock_df['Unnamed: 0']]
+
+        self.factory_names = [str(c) for c in \
+            self.production_capacity_df.columns][1:]
+
+        self.material_names = [str(c) for c in \
+            self.product_requirements_df.columns][1:]
 
         self.solver = pywraplp.Solver(\
                         'LPWrapper',\
-                        pywraplp.Solver.GLPK_LINEAR_PROGRAMMING)
+                        pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
+
+
+    def replace_nans(self, df:pd.DataFrame, newValue:int):
+        df.replace(float('nan'), float(newValue), inplace=True)
+        print(df)
 
 
     def read_csv(self, sheet_name:str)->pd.DataFrame:
-        return pd.read_excel(self.excel_file_name, sheet_name=sheet_name)
+        df = pd.read_excel(self.excel_file_name, sheet_name=sheet_name)
+        print()
+        print(sheet_name)
+        print(df)
+        print()
+        print()
+        return df
+
 
 
 def t1_main()->None:
